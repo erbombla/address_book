5abfd66948314526b55a424d6721a3d5adcfe710
Eric Bomblatus and Ryan Back <eric@>
2016-12-01 11:45:05 -0800

    add specs for Address methods and pass

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -3,6 +3,9 @@ require 'address'
 require 'contact'
 
 describe 'Address' do
+  # before do
+  #   Address.clear
+  # end
   describe '#type' do
     it 'displays address type' do
       new_address = Address.new({:type => 'home', :address => '409 Tork Place', :city => 'Hillsboro', :state => 'OR', :zip_code => 97006, :country => 'United States'})
@@ -13,6 +16,30 @@ describe 'Address' do
     it 'displays address' do
       new_address = Address.new({:type => 'home', :address => '409 Tork Place', :city => 'Hillsboro', :state => 'OR', :zip_code => 97006, :country => 'United States'})
       expect(new_address.address).to eq('409 Tork Place')
+    end
+  end
+  describe '#city' do
+    it 'displays city' do
+      new_address = Address.new({:type => 'home', :address => '409 Tork Place', :city => 'Hillsboro', :state => 'OR', :zip_code => 97006, :country => 'United States'})
+      expect(new_address.city).to eq('Hillsboro')
+    end
+  end
+  describe '#state' do
+    it 'displays state' do
+      new_address = Address.new({:type => 'home', :address => '409 Tork Place', :city => 'Hillsboro', :state => 'OR', :zip_code => 97006, :country => 'United States'})
+      expect(new_address.state).to eq('OR')
+    end
+  end
+  describe '#zip_code' do
+    it 'displays zip code' do
+      new_address = Address.new({:type => 'home', :address => '409 Tork Place', :city => 'Hillsboro', :state => 'OR', :zip_code => 97006, :country => 'United States'})
+      expect(new_address.zip_code).to eq(97006)
+    end
+  end
+  describe '#country' do
+    it 'displays country' do
+      new_address = Address.new({:type => 'home', :address => '409 Tork Place', :city => 'Hillsboro', :state => 'OR', :zip_code => 97006, :country => 'United States'})
+      expect(new_address.country).to eq('United States')
     end
   end
 end
